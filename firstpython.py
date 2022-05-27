7295891ee68040500027cdfacd9d6a58f5d40050
karmen1102 <63945953+karmen1102@users.noreply.github.com>
2022-05-27 14:01:58 +0800

    firstpython.py
    link

---
+++ firstpython.py
@@ -0,0 +1,3 @@
+# Display the output
+print("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0221EN-SkillsNetwork/labs/module%206/Final%20Assignment/ETL_Engineer_Peer_Review_Assignment.ipynb")
+
